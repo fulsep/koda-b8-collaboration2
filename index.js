98d5450affe5d3f62b7ad76df51a188ef5df5772
Zacky Rafian Fawwauzy <imftcy@gmail.com>
2026-05-25 17:41:28 +0700

    docs: add documentation in palindrome function

---
+++ index.js
@@ -0,0 +1,10 @@
+const isPalindrome = require("./lib/palindrome.js")
+
+const result = [ 
+  isPalindrome("racecar"),
+  isPalindrome("duud"),
+  isPalindrome("tes"),
+  isPalindrome("Ghammma")
+]
+
+console.log(result);
