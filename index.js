cdcfdf2891de62525f9adc0b1663a09ba8c80f02
MuhamadRafliShidiq <130623661+MuhamadRafliShidiq@users.noreply.github.com>
2026-05-25 17:54:47 +0700

    doc: add JSDoc in function isPalindrome

---
+++ index.js
@@ -0,0 +1,11 @@
+const isPalindrome = require('./lib/palindrome.js');
+
+const result = [
+    isPalindrome('radar'),
+    isPalindrome('Radar'),
+    isPalindrome('LeVeL'),
+    isPalindrome('hello'),
+    isPalindrome('world'),
+
+];
+console.log(result);
